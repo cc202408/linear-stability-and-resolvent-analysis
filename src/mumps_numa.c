91b146484cab40ad61c97d1969bd95dfa9ac2e2d
admin191 <luchen202211@gmail.com>
2023-04-14 01:19:26 +0800

    first commit

---
+++ src/mumps_numa.c
@@ -0,0 +1,19 @@
+/*
+ *
+ *  This file is part of MUMPS 5.5.1, released
+ *  on Tue Jul 12 13:17:24 UTC 2022
+ *
+ *
+ *  Copyright 1991-2022 CERFACS, CNRS, ENS Lyon, INP Toulouse, Inria,
+ *  Mumps Technologies, University of Bordeaux.
+ *
+ *  This version of MUMPS is provided to you free of charge. It is
+ *  released under the CeCILL-C license 
+ *  (see doc/CeCILL-C_V1-en.txt, doc/CeCILL-C_V1-fr.txt, and
+ *  https://cecill.info/licences/Licence_CeCILL-C_V1-en.html)
+ *
+ */
+int mumps_numa_return()
+{
+return 0;
+}
---
+++ src/mumps_thread_affinity.c
@@ -0,0 +1,23 @@
+/*
+ *
+ *  This file is part of MUMPS 5.5.1, released
+ *  on Tue Jul 12 13:17:24 UTC 2022
+ *
+ *
+ *  Copyright 1991-2022 CERFACS, CNRS, ENS Lyon, INP Toulouse, Inria,
+ *  Mumps Technologies, University of Bordeaux.
+ *
+ *  This version of MUMPS is provided to you free of charge. It is
+ *  released under the CeCILL-C license 
+ *  (see doc/CeCILL-C_V1-en.txt, doc/CeCILL-C_V1-fr.txt, and
+ *  https://cecill.info/licences/Licence_CeCILL-C_V1-en.html)
+ *
+ */
+#include "mumps_thread_affinity.h"
+void MUMPS_CALL
+MUMPS_THREAD_AFFINITY_RETURN()
+{
+  /*
+   * Thread affinity tools will be available in the future.
+   */
+}
